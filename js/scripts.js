5a9ab33196665f7f54965e57f6ae2a93d30b6a3c
diegosalazar343 <diegosalazar343@gmail.com>
2021-01-22 13:35:15 -0800

    attempt to get beep from input 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
+//Business Logic
 function numbers(user){
   let newArray = [];
   for(let i=0; i<= user; i++) {
     newArray.push(i.toString());
-  };
+  }
+
+  let input=0;
+  newArray.forEach(function(number)) {
+    if(number.indexOf('1')) {
+      newArray[input] = "Beep!";
+    }
+  }
+
   const arrayResult = newArray.join(" , ");
   return arrayResult;
   console.log(arrayResult);
 };
+
+//User Logic
